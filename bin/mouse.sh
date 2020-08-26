8e5de4a969334f00b3bc4c5b10d7a6df4395aeaf
Daniel Sjöstrand <daniel@bastec.se>
2020-08-26 10:43:57 +0200

    Update config, mount.sh, and 4 more files...

--- bin/mouse.sh
+++ bin/mouse.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-xinput set-prop 12 177 2.5 0 0 0 2.5 0 0 0 1
-xinput set-prop 12 320 0.2
+xinput set-prop 15 177 2.5 0 0 0 2.5 0 0 0 1
+xinput set-prop 15 320 0.2
+
+# xinput list # look for device number (Microsoft intelli), ex. 15
+# xinput list-props 15
